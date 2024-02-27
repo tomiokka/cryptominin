3256de7815315914db2a65971508acdaea46c8b8
tensa <127889162+tomiokka@users.noreply.github.com>
2024-02-28 00:34:09 +0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import AppLayout from "./components/Layout/AppLayout";
+import {CryptoContextProvider} from "./context/crypto-context";
+
+
+export default function App() {
+  return (
+    <CryptoContextProvider>
+    <AppLayout/>
+    </CryptoContextProvider>
+  );
+}
